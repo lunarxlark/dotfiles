00aae3ee620d4bdffc73773e765fa60232afdadc
lunarxlark <lunarxlark@gmail.com>
2021-08-08 10:12:11 +0900

    fix

---
+++ init.sh
@@ -0,0 +1,5 @@
+ln -sf ${GHQ_ROOT}/github.com/lunarxlark/dotfiles/.vim ${XDG_DATA_HOME}/nvim/site
+ln -sf ${GHQ_ROOT}/github.com/lunarxlark/dotfiles/.vimrc ${HOME}/.vimrc
+ln -sf ${GHQ_ROOT}/github.com/lunarxlark/dotfiles/.gvimrc ${HOME}/.gvimrc
+ln -sf ${GHQ_ROOT}/github.com/lunarxlark/dotfiles/.gitconfig ${HOME}/.gitconfig
+ln -sf ${GHQ_ROOT}/github.com/lunarxlark/dotfiles/.tmux.conf ${HOME}/.tmux.conf
