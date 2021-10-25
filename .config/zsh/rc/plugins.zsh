217ad57b5ea9eadeb4e26de0c07970c8b76628ce
lunarxlark <lunarxlark@gmail.com>
2021-10-26 02:37:22 +0900

    update zsh

---
+++ .config/zsh/rc/plugins.zsh
@@ -0,0 +1,123 @@
+#------------------------------------------------------------------------------#
+# Plugins                                                                      #
+#------------------------------------------------------------------------------#
+### Added by Zinit's installer {{{
+if [[ ! -f $HOME/.zinit/bin/zinit.zsh ]]; then
+    print -P "%F{33}▓▒░ %F{220}Installing %F{33}DHARMA%F{220} Initiative Plugin Manager (%F{33}zdharma/zinit%F{220})…%f"
+    command mkdir -p "$HOME/.zinit" && command chmod g-rwX "$HOME/.zinit"
+    command git clone https://github.com/zdharma/zinit "$HOME/.zinit/bin" && \
+        print -P "%F{33}▓▒░ %F{34}Installation successful.%f%b" || \
+        print -P "%F{160}▓▒░ The clone has failed.%f%b"
+fi
+
+source "$HOME/.zinit/bin/zinit.zsh"
+autoload -Uz _zinit
+(( ${+_comps} )) && _comps[zinit]=_zinit
+
+# Load a few important annexes, without Turbo
+# (this is currently required for annexes)
+zinit light-mode for \
+    zinit-zsh/z-a-rust \
+    zinit-zsh/z-a-as-monitor \
+    zinit-zsh/z-a-patch-dl \
+    zinit-zsh/z-a-bin-gem-node
+
+### }}} End of Zinit's installer chunk
+
+
+# Load starship theme
+# starship
+export STARSHIP_CONFIG=$XDG_CONFIG_HOME/starship/starship.toml
+export STARSHIP_CACHE=$XDG_CACHE_HOME/starship
+zinit ice lucid \
+  from"gh-r" \
+  as"command" \
+  atclone"./starship init zsh > init.zsh; ./starship completions zsh > _starship" \
+  atpull"%atclone" \
+  src"init.zsh"
+zinit light starship/starship
+
+# fzf
+zinit snippet https://github.com/junegunn/fzf/blob/master/shell/key-bindings.zsh
+zinit snippet https://github.com/junegunn/fzf/blob/master/shell/completion.zsh
+zinit ice \
+  as"program" \
+  from"gh-r"
+zinit light junegunn/fzf-bin
+
+# github
+zinit wait'1' lucid \
+  from"gh-r" \
+  as"program" \
+  pick"ghq*/ghq" \
+  light-mode for @x-motemen/ghq
+
+zinit wait'1' lucid \
+  from"gh-r" \
+  as'program' \
+  pick'**/gh' \
+  light-mode for @cli/cli
+
+# syntax highlight
+zinit light zsh-users/zsh-syntax-highlighting
+
+# direnv
+zinit wait'!1' lucid \
+  from"gh-r" \
+  as"program" \
+  mv"direnv* -> direnv" \
+  pick"direnv" \
+  atclone'./direnv hook zsh > zhook.zsh' \
+  atpull'%atclone' \
+  src'zhook.zsh' \
+  light-mode for @direnv/direnv
+
+export ASDF_CONFIG_FILE=$XDG_CONFIG_HOME/asdf/config
+export ASDF_DIR=$XDG_DATA_HOME/asdf
+export ASDF_DATA_DIR=$XDG_DATA_HOME/asdf
+zinit wait'1' lucid \
+  pick"asdf.sh" \
+  light-mode for @asdf-vm/asdf
+
+# delta
+zinit wait'1' lucid \
+  from"gh-r" \
+  as"program" \
+  pick"delta*/delta" \
+  light-mode for @dandavison/delta
+
+# ripgrep
+zinit wait'1' lucid blockf nocompletions \
+  from"gh-r" \
+  as'program' \
+  pick'ripgrep*/rg' \
+  atclone'chown -R $(id -nu):$(id -ng) .; zinit creinstall -q BurntSushi/ripgrep' \
+  atpull'%atclone' \
+  light-mode for @BurntSushi/ripgrep
+
+# fd
+zinit wait'1' lucid blockf nocompletions \
+  from"gh-r" \
+  as'program' \
+  cp"fd-*/autocomplete/_fd -> _fd" \
+  pick'fd*/fd' \
+  atclone'chown -R $(id -nu):$(id -ng) .; zinit creinstall -q sharkdp/fd' \
+  atpull'%atclone' \
+  light-mode for @sharkdp/fd
+
+# bat
+zinit wait'1' lucid \
+  from"gh-r" \
+  as"program" \
+  cp"bat/autocomplete/bat.zsh -> _bat" \
+  pick"bat*/bat" \
+  atload"export BAT_THEME='gruvbox-dark'; alias cat=bat" \
+  light-mode for @sharkdp/bat
+
+# wakatime
+zinit load 'wbingli/zsh-wakatime'
+
+# completion
+zinit wait lucid blockf \
+  atload"zicompinit; zicdreplay" \
+  for zsh-users/zsh-completions
--- .zshenv
+++ .zshenv
@@ -1,5 +1,46 @@
-source $HOME/env.sh
-HISTFILE=~/.zsh_history
-HISTSIZE=1000000
-SAVEHIST=1000000
-FZF_DEFAULT_COMMAND='fd --type f'
+#------------------------------------------------------------------------------#
+# Environment variable                                                         #
+#------------------------------------------------------------------------------#
+export LANG=ja_JP.UTF-8
+if builtin command -v nvim > /dev/null 2>&1; then
+  export EDITOR=${EDITOR:-nvim}
+else
+  export EDITOR=${EDITOR:-vim}
+fi
+
+export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
+export XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.data}"
+export XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.cache}"
+
+# zsh & zinit
+export ZDOTDIR=$HOME
+export ZHOMEDIR=$ZDOTDIR/.config/zsh
+export ZRCDIR=$ZHOMEDIR/rc
+export ZDATADIR=$XDG_DATA_HOME/zsh
+
+# go
+export GO111MODULE=on
+export GOPATH=$HOME/dev
+export GOBIN=$GOPATH/bin
+export GOENV=$GOPATH/env
+export GOCACHE=$GOPATH/go-build
+# GOROOTはgolang.org/dlで設定されたデフォルト値を変えられない
+# そのため、defaultのまま。
+#export GOROOT=$HOME/dev
+
+# ghq
+export GHQ_ROOT=$GOPATH/src
+
+# rustup
+. "$HOME/.cargo/env"
+
+export PATH=~/Library/Python/3.7/bin:$PATH
+
+path=(
+  $HOME/.local/bin
+  $HOME/.local/go/bin
+  $GOBIN
+  $path
+  )
+
+# }}}
