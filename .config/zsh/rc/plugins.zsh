b0255ed6ed400f0b0a10758d99c2eb6dd5e82bb5
lunarxlark <lunarxlark@gmail.com>
2021-11-05 15:21:29 +0900

    remove lazygit

--- .config/zsh/rc/plugins.zsh
+++ .config/zsh/rc/plugins.zsh
@@ -45,13 +45,6 @@ zinit ice \
   from"gh-r"
 zinit light junegunn/fzf-bin
 
-# git
-zinit wait'1' lucid \
-  from"gh-r" \
-  as"program" \
-  pick"lazygit*/lazygit" \
-  light-mode for @jesseduffield/lazygit
-
 # github
 zinit wait'1' lucid \
   from"gh-r" \
