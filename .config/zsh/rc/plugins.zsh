df54ec046a0bcdff91dfadc15188f05fcfea24b8
lunarxlark <lunarxlark@gmail.com>
2021-11-05 14:58:11 +0900

    fix repo url for z-shell/zinit

--- .config/zsh/rc/plugins.zsh
+++ .config/zsh/rc/plugins.zsh
@@ -3,9 +3,9 @@
 #------------------------------------------------------------------------------#
 ### Added by Zinit's installer {{{
 if [[ ! -f $HOME/.zinit/bin/zinit.zsh ]]; then
-    print -P "%F{33}▓▒░ %F{220}Installing %F{33}DHARMA%F{220} Initiative Plugin Manager (%F{33}zdharma/zinit%F{220})…%f"
+    print -P "%F{33}▓▒░ %F{220}Installing %F{33}Z-shell%F{220} Initiative Plugin Manager (%F{33}z-shell/zinit%F{220})…%f"
     command mkdir -p "$HOME/.zinit" && command chmod g-rwX "$HOME/.zinit"
-    command git clone https://github.com/zdharma/zinit "$HOME/.zinit/bin" && \
+    command git clone https://github.com/z-shell/zinit "$HOME/.zinit/bin" && \
         print -P "%F{33}▓▒░ %F{34}Installation successful.%f%b" || \
         print -P "%F{160}▓▒░ The clone has failed.%f%b"
 fi
@@ -17,10 +17,10 @@ autoload -Uz _zinit
 # Load a few important annexes, without Turbo
 # (this is currently required for annexes)
 zinit light-mode for \
-    zinit-zsh/z-a-rust \
-    zinit-zsh/z-a-as-monitor \
-    zinit-zsh/z-a-patch-dl \
-    zinit-zsh/z-a-bin-gem-node
+    z-shell/z-a-rust \
+    z-shell/z-a-as-monitor \
+    z-shell/z-a-patch-dl \
+    z-shell/z-a-bin-gem-node
 
 ### }}} End of Zinit's installer chunk
 
