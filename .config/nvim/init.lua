fe2214cee0d0c4007154b92d2dc7716391dfe25f
lunarxlark <lunarxlark@gmail.com>
2021-11-22 10:50:53 +0900

    使っていない引数削除

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -140,7 +140,7 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-local on_attach = function(client, bufnr)
+local on_attach = function(_, bufnr)
 local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
 local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
   buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
