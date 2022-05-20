854899f318d0a1bc58996f78a7ed889ec01d301a
lunarxlark <lunarxlark@gmail.com>
2022-05-20 11:38:32 +0900

    miss config for toggleterm, jaq

---
+++ .config/nvim/lua/config/toggleterm.lua
@@ -0,0 +1,13 @@
+require("toggleterm").setup {
+  direction = 'float',
+  float_opts = {
+    -- The border key is *almost* the same as 'nvim_open_win'
+    -- see :h nvim_open_win for details on borders however
+    -- the 'curved' border is a custom border type
+    -- not natively supported but implemented in this plugin.
+    border = 'single',
+    width = 80,
+    height = 80,
+    winblend = 3,
+  }
+}
