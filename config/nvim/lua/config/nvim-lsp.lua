146f8aa808d28e286521404dbdcab30741f50830
lunarxlark <lunarxlark@gmail.com>
2022-05-29 23:50:29 +0900

    daemonize for gopls

--- config/nvim/lua/config/nvim-lsp.lua
+++ config/nvim/lua/config/nvim-lsp.lua
@@ -86,7 +86,7 @@ lspconfig.sumneko_lua.setup {
 lspconfig.gopls.setup {
   on_attach = on_attach,
   capabilities = capabilities,
-  cmd = { lsp_installer_dir .. '/gopls/gopls', 'serve'},
+  cmd = { lsp_installer_dir .. '/gopls/gopls', '-remote=:37374', '-remote.logfile=/tmp/gopls.remote.log'},
   flags = {
     debounce_text_changes = 150,
   },
