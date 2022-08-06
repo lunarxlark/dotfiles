840f59b41ea9953d23d00f6f0adae85a0255892a
lunarxlark <lunarxlark@gmail.com>
2022-08-06 22:34:23 +0900

    update

---
+++ config/nvim/lua/config/alpha.lua
@@ -0,0 +1,89 @@
+local job = require('plenary')
+
+local present, alpha = pcall(require, "alpha")
+if not present then
+	return
+end
+
+local function button(sc, txt, keybind)
+	local sc_ = sc:gsub("%s", ""):gsub("SPC", "<leader>")
+
+	local opts = {
+		position = "center",
+		text = txt,
+		shortcut = sc,
+		cursor = 6,
+		width = 19,
+		align_shortcut = "right",
+		hl_shortcut = "Number",
+		hl = "Function",
+	}
+	if keybind then
+		opts.keymap = { "n", sc_, keybind, { noremap = true, silent = true } }
+	end
+
+	return {
+		type = "button",
+		val = txt,
+		on_press = function()
+			local key = vim.api.nvim_replace_termcodes(sc_, true, false, true)
+			vim.api.nvim_feedkeys(key, "normal", false)
+		end,
+		opts = opts,
+	}
+end
+
+local buttons = {
+	type = "group",
+	val = {
+    button("e", "   New file", ":ene <BAR> startinsert <CR>"),
+		button("r", "   Recents", ":Telescope oldfiles<CR>"),
+		button("f", "   Search", ":Telescope find_files<CR>"),
+		button("u", "   Update", ":PackerSync<CR>"),
+		button("c", "   Config", ":Telescope find_files search_dirs=$XDG_CONFIG_HOME/nvim<CR>"),
+		button("q", "   Quit", ":qa!<CR>"),
+	},
+	opts = {
+		position = "center",
+	},
+}
+
+local hydra = {
+    "                                   ",
+    "                                   ",
+    "                                   ",
+    "   ⣴⣶⣤⡤⠦⣤⣀⣤⠆     ⣈⣭⣿⣶⣿⣦⣼⣆          ",
+    "    ⠉⠻⢿⣿⠿⣿⣿⣶⣦⠤⠄⡠⢾⣿⣿⡿⠋⠉⠉⠻⣿⣿⡛⣦       ",
+    "          ⠈⢿⣿⣟⠦ ⣾⣿⣿⣷    ⠻⠿⢿⣿⣧⣄     ",
+    "           ⣸⣿⣿⢧ ⢻⠻⣿⣿⣷⣄⣀⠄⠢⣀⡀⠈⠙⠿⠄    ",
+    "          ⢠⣿⣿⣿⠈    ⣻⣿⣿⣿⣿⣿⣿⣿⣛⣳⣤⣀⣀   ",
+    "   ⢠⣧⣶⣥⡤⢄ ⣸⣿⣿⠘  ⢀⣴⣿⣿⡿⠛⣿⣿⣧⠈⢿⠿⠟⠛⠻⠿⠄  ",
+    "  ⣰⣿⣿⠛⠻⣿⣿⡦⢹⣿⣷   ⢊⣿⣿⡏  ⢸⣿⣿⡇ ⢀⣠⣄⣾⠄   ",
+    " ⣠⣿⠿⠛ ⢀⣿⣿⣷⠘⢿⣿⣦⡀ ⢸⢿⣿⣿⣄ ⣸⣿⣿⡇⣪⣿⡿⠿⣿⣷⡄  ",
+    " ⠙⠃   ⣼⣿⡟  ⠈⠻⣿⣿⣦⣌⡇⠻⣿⣿⣷⣿⣿⣿ ⣿⣿⡇ ⠛⠻⢷⣄ ",
+    "      ⢻⣿⣿⣄   ⠈⠻⣿⣿⣿⣷⣿⣿⣿⣿⣿⡟ ⠫⢿⣿⡆     ",
+    "       ⠻⣿⣿⣿⣿⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⡟⢀⣀⣤⣾⡿⠃     ",
+    "                                   ",
+  }
+
+local heading = {
+  type = "text",
+  val = hydra,
+  opts = {
+    position = "center",
+    hl = "AlphaHeading",
+  },
+}
+
+local layout = {
+  heading,
+  { type = "padding", val = 2 },
+  buttons,
+}
+
+local config = {
+  layout = layout,
+  opts = { margin = 10 },
+}
+
+alpha.setup(config)
---
+++ config/wezterm/wezterm.lua
@@ -0,0 +1,49 @@
+local wezterm = require("wezterm")
+
+local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+local SOLID_RIGHT_ARROW = utf8.char(0xe0b0)
+
+wezterm.on("update-right-status", function(window, pane)
+	local success, date, stderr = wezterm.run_child_process({ "date" })
+	date = wezterm.strftime("%Y/%m/%d %H:%M")
+
+	window:set_right_status(wezterm.format({
+		{ Text = date },
+	}))
+end)
+
+return {
+	color_scheme = "Gruvbox Dark",
+	font = wezterm.font("MesloLGM Nerd Font", { weight = "Regular", stretch = "Normal", style = "Normal" }),
+	font_size = 14.0,
+	adjust_window_size_when_changing_font_size = false,
+	disable_default_key_bindings = true,
+	use_ime = true,
+	tab_bar_at_bottom = false,
+	use_fancy_tab_bar = false,
+	-- window_decorations = "NONE",
+	-- tab_bar_style = {
+	-- 	active_tab_left = wezterm.format({
+	-- 		{ Background = { Color = "#002240" } },
+	-- 		{ Foreground = { Color = "#2cc55d" } },
+	-- 		{ Text = SOLID_LEFT_ARROW },
+	-- 	}),
+	-- 	active_tab_right = wezterm.format({
+	-- 		{ Background = { Color = "#002240" } },
+	-- 		{ Foreground = { Color = "#2cc55d" } },
+	-- 		{ Text = SOLID_RIGHT_ARROW },
+	-- 	}),
+	-- 	inactive_tab_left = wezterm.format({
+	-- 		{ Background = { Color = "#002240" } },
+	-- 		{ Foreground = { Color = "#2cc55d" } },
+	-- 		{ Text = SOLID_LEFT_ARROW },
+	-- 	}),
+	-- 	inactive_tab_right = wezterm.format({
+	-- 		{ Background = { Color = "#002240" } },
+	-- 		{ Foreground = { Color = "#2cc55d" } },
+	-- 		{ Text = SOLID_RIGHT_ARROW },
+	-- 	}),
+	-- },
+	leader = { key = "t", mods = "CTRL", timeout_milliseconds = 1000 },
+	keys = require("keymap"),
+}
