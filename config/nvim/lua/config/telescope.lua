808640bdda15cc7da978ea6e8f1b6f28675ff8fe
lunarxlark <lunarxlark@gmail.com>
2022-09-13 21:40:31 +0900

    nvim telescope : multi selection

--- config/nvim/lua/config/telescope.lua
+++ config/nvim/lua/config/telescope.lua
@@ -6,6 +6,23 @@ end
 
 local actions = require("telescope.actions")
 local actions_layout = require("telescope.actions.layout")
+local action_state = require("telescope.actions.state")
+
+local custom_actions = {}
+
+function custom_actions.fzf_multi_select(prompt_bufnr)
+  local picker = action_state.get_current_picker(prompt_bufnr)
+  local num_selections = #picker:get_multi_selection()
+
+  if num_selections > 1 then
+    -- actions.file_edit throws - context of picker seems to change
+    --actions.file_edit(prompt_bufnr)
+    actions.send_selected_to_qflist(prompt_bufnr)
+    actions.open_qflist()
+  else
+    actions.file_edit(prompt_bufnr)
+  end
+end
 
 telescope.setup({
   defaults = {
@@ -13,6 +30,9 @@ telescope.setup({
       i = {
         ["<c-y>"] = actions_layout.toggle_preview,
         ["<esc>"] = actions.close,
+        ["<tab>"] = actions.toggle_selection + actions.move_selection_next,
+        ["<s-tab>"] = actions.toggle_selection + actions.move_selection_previous,
+        ["<cr>"] = custom_actions.fzf_multi_select,
       },
     },
     color_devicons = true,
