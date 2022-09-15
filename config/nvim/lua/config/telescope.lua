1427955de93ad8f2ece2729349ec89e4b96343a5
lunarxlark <lunarxlark@gmail.com>
2022-09-15 21:49:56 +0900

    nvim telescope:delete multi select

--- config/nvim/lua/config/telescope.lua
+++ config/nvim/lua/config/telescope.lua
@@ -8,22 +8,6 @@ local actions = require("telescope.actions")
 local actions_layout = require("telescope.actions.layout")
 local action_state = require("telescope.actions.state")
 
-local custom_actions = {}
-
-function custom_actions.fzf_multi_select(prompt_bufnr)
-  local picker = action_state.get_current_picker(prompt_bufnr)
-  local num_selections = #picker:get_multi_selection()
-
-  if num_selections > 1 then
-    -- actions.file_edit throws - context of picker seems to change
-    --actions.file_edit(prompt_bufnr)
-    actions.send_selected_to_qflist(prompt_bufnr)
-    actions.open_qflist()
-  else
-    actions.file_edit(prompt_bufnr)
-  end
-end
-
 telescope.setup({
   defaults = {
     mappings = {
