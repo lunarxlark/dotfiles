7f084ce0ffc95a02062ad1ad6d5be2f2d75c539c
lunarxlark <lunarxlark@gmail.com>
2022-12-23 17:36:56 +0900

    nvim indent: don't show context

--- config/nvim/lua/config/plugins/indent-blankline.lua
+++ config/nvim/lua/config/plugins/indent-blankline.lua
@@ -16,8 +16,8 @@ function M.config()
       "IndentBlanklineIndent2",
     },
     show_trailing_blankline_indent = false,
-    show_current_context = true,
-    show_current_context_start = true,
+    show_current_context = false,
+    show_current_context_start = false,
   })
 end
 
