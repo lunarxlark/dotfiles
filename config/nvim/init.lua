73f3592b0ffce268edf1d8af374a488509f5b344
obsidian <obsidian@noreply>
2023-01-04 21:15:53 +0900

    nvim lazy:separate lazy.config

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,20 +1,5 @@
 vim.g.mapleader = " "
 
-require("option")
-
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "--single-branch",
-    "https://github.com/folke/lazy.nvim.git",
-    lazypath,
-  })
-end
-vim.opt.runtimepath:prepend(lazypath)
-
-require("lazy").setup("config.plugins", {
-  install = {},
-})
+require("config.option")
+require("config.keymap")
+require("config.lazy")
