50cc555c0f23122a699bb99e6ad6e226fcb7bd63
obsidian <obsidian@noreply>
2023-01-04 22:50:17 +0900

    nvim treesitter-context:add new plugin

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,52 +1,59 @@
-local M = {
-  "nvim-treesitter/nvim-treesitter",
-  dev = false,
-  build = ":TSUpdate",
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    dev = false,
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = {
+          "comment",
+          "bash",
+          "cmake",
+          "make",
+          "dockerfile",
+          "dot",
+          "http",
+          "html",
+          "go",
+          "gomod",
+          "json",
+          "jsonc",
+          "lua",
+          "php",
+          "python",
+          "regex",
+          "markdown",
+          "markdown_inline",
+          "sql",
+          "tsx",
+          "javascript",
+          "typescript",
+          "toml",
+          "svelte",
+          "hcl",
+          "vim",
+          "yaml",
+        },
+        sync_install = false,
+        highlight = {
+          enable = true,
+        },
+        refactor = {
+          highlight_definitions = { enable = true },
+        },
+        -- experimental
+        indent = {
+          enable = true,
+        },
+      })
+    end,
+  },
+
+  {
+
+    "nvim-treesitter/nvim-treesitter-context",
+    config = function()
+      require("treesitter-context").setup()
+    end,
+  },
 }
-
-function M.config()
-  require("nvim-treesitter.configs").setup({
-    ensure_installed = {
-      "comment",
-      "bash",
-      "cmake",
-      "make",
-      "dockerfile",
-      "dot",
-      "http",
-      "html",
-      "go",
-      "gomod",
-      "json",
-      "jsonc",
-      "lua",
-      "php",
-      "python",
-      "regex",
-      "markdown",
-      "markdown_inline",
-      "sql",
-      "tsx",
-      "javascript",
-      "typescript",
-      "toml",
-      "svelte",
-      "hcl",
-      "vim",
-      "yaml",
-    },
-    sync_install = false,
-    highlight = {
-      enable = true,
-    },
-    refactor = {
-      highlight_definitions = { enable = true },
-    },
-    -- experimental
-    indent = {
-      enable = true,
-    },
-  })
-end
-
-return M
