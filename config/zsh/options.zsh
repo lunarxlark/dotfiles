a20b3ceea975994ff4348b6835f62ef25d4bcd58
obsidian <obsidian@noreply>
2023-01-05 12:36:01 +0900

    zsh options:add share history

--- config/zsh/options.zsh
+++ config/zsh/options.zsh
@@ -3,9 +3,10 @@
 #------------------------------------------------------------------------------#
 setopt prompt_subst          # プロンプトに escape sequence (環境変数) を通す
 
-setopt append_history        # 履歴を追加 (毎回 .zhistory を作るのではなく)
+setopt share_history
+setopt inc_append_history    # 履歴を追加 (毎回 .zhistory を作るのではなく)
+setopt hist_reduce_blanks
 setopt hist_ignore_all_dups  # 重複するコマンド行は古い方を削除
 setopt hist_ignore_dups      # 直前と同じコマンドラインはヒストリに追加しない
 setopt hist_ignore_space     # スペースで始まるコマンド行はヒストリリストから削除
 setopt hist_no_store         # historyコマンドは履歴に登録しない
-setopt share_history
