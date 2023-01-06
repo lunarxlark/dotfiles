13f89bfbbea58fa72a95ddc314a78c0c7e88072c
obsidian <obsidian@noreply>
2023-01-06 18:20:08 +0900

    fix(gruvbox):override Diffxxx

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -15,6 +15,12 @@ return {
     config = function()
       require("gruvbox").setup({
         italic = false,
+        overrides = {
+          DiffAdd = { fg = "#182819", bg = "#b8bb26" },
+          DiffChange = { fg = "#182819", bg = "#8ec07c" },
+          DiffDelete = { fg = "#210A07", bg = "#fb4934" },
+          DiffText = { fg = "#291F08", bg = "#fabd2f" },
+        },
       })
       require("gruvbox").load()
     end,
