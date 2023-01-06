f158c4bf2f69a436f4bb7b8f988dbe286141d863
obsidian <obsidian@noreply>
2023-01-06 18:44:30 +0900

    feat(diffview):lazy load

--- config/nvim/lua/plugins/diffview.lua
+++ config/nvim/lua/plugins/diffview.lua
@@ -3,6 +3,8 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
   },
+
+  cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles" },
   config = function()
     require("diffview").setup({
       enhanced_diff_hl = false,
