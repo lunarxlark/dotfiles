10d2cd5e901884a134525b544ccc44d3c150b3ff
obsidian <obsidian@noreply>
2023-01-06 18:50:36 +0900

    feat(tig):delete init

--- config/nvim/lua/plugins/tig.lua
+++ config/nvim/lua/plugins/tig.lua
@@ -1,14 +1,13 @@
 return {
   "iberianpig/tig-explorer.vim",
 
-  cmd = { "TigOpenProjectRootDir", "TigBlame" },
   dependencies = {
     "rbgrouleff/bclose.vim",
   },
-
-  init = function()
-    vim.keymap.set("n", "<leader>T", "<cmd>TigOpenProjectRootDir<cr>", { desc = "tig open" })
-    vim.keymap.set("n", "<leader>Tc", "<cmd>TigOpenCurrentFile<cr>", { desc = "tig open %" })
-    vim.keymap.set("n", "<leader>Tb", "<cmd>TigBlame<cr>", { desc = "tig blame" })
-  end,
+  cmd = { "TigOpenProjectRootDir", "TigBlame" },
+  keys = {
+    { "<leader>T", "<cmd>TigOpenProjectRootDir<cr>", { desc = "tig open" } },
+    { "<leader>Tc", "<cmd>TigOpenCurrentFile<cr>", { desc = "tig open %" } },
+    { "<leader>Tb", "<cmd>TigBlame<cr>", { desc = "tig blame" } },
+  },
 }
