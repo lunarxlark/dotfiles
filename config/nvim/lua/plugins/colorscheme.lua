76df3f48d18f64d3b5e021743072e509b7eb1298
lunarxlark <lunarxlark@gmail.com>
2023-01-09 00:37:07 +0900

    feat(nightfox):adapt nordfox

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -11,34 +11,7 @@ return {
     "EdenEast/nightfox.nvim",
     config = function()
       require("nightfox").setup()
-      require("nightfox").load()
+      vim.cmd([[colorscheme nordfox]])
     end,
   },
-  -- {
-  --   "ellisonleao/gruvbox.nvim",
-  --   lazy = false,
-  --   config = function()
-  --     require("gruvbox").setup({
-  --       italic = true,
-  --       -- inverse = true,
-  --       overrides = {
-  --         DiffAdd = { fg = "#182819", bg = "#b8bb26" },
-  --         DiffChange = { fg = "#182819", bg = "#8ec07c" },
-  --         DiffDelete = { fg = "#210A07", bg = "#fb4934" },
-  --         DiffText = { fg = "#291F08", bg = "#fabd2f" },
-  --         GitSignsChangeLnInline = { fg = "#b8bb26", bg = "#182819" },
-  --         GitSignsAddLnInline = { fg = "#b8bb26", bg = "#182819" },
-  --       },
-  --     })
-  --     -- require("gruvbox").load()
-  --   end,
-  -- },
-  -- {
-  --   "gruvbox.nvim",
-  --   dev = true,
-  --   config = function()
-  --     require("gruvbox").setup()
-  --     -- require("gruvbox").load()
-  --   end,
-  -- },
 }
