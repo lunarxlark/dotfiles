9314b0445aa08d295d01a8e0b49d3c6bb98f0a8a
lunarxlark <lunarxlark@gmail.com>
2023-01-09 00:48:59 +0900

    feat(lualine):use nordfox

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -7,7 +7,7 @@ function M.config()
   require("lualine").setup({
     options = {
       icons_enabled = true,
-      theme = "gruvbox",
+      theme = "nordfox",
       section_separators = { left = "", right = "" },
       component_separators = { left = "", right = "" },
     },
