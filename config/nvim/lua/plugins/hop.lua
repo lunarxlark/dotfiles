306ec6f1627bb50af4715a78ede09bc78f860d9d
lunarxlark <lunarxlark@gmail.com>
2023-01-08 22:51:42 +0900

    feat(hop):delete init function and lazy load

--- config/nvim/lua/plugins/hop.lua
+++ config/nvim/lua/plugins/hop.lua
@@ -1,19 +1,16 @@
-local M = {
+return {
   "phaazon/hop.nvim",
+
   event = "BufReadPost",
+  keys = {
+    { "f", "<cmd>HopChar1CurrentLineAC<CR>", { desc = "search char1 on in current line (forward)" } },
+    { "F", "<cmd>HopChar1CurrentLineBC<CR>", { desc = "search char1 on in current line (beforeward)" } },
+    { "<leader>w", "<cmd>HopWordAC<CR>", { desc = "search word (forward)" } },
+    { "<leader>W", "<cmd>HopWordBC<CR>", { desc = "search word (backward)" } },
+    { "<leader>c", "<cmd>HopWordCurrentLineAC<CR>", { desc = "search word in current line (forward)" } },
+    { "<leader>C", "<cmd>HopWordCurrentLineBC<CR>", { desc = "search word in current line (forward)" } },
+  },
+  config = function()
+    require("hop").setup()
+  end,
 }
-
-function M.config()
-  require("hop").setup()
-end
-
-function M.init()
-  vim.keymap.set("n", "f", "<cmd>HopChar1CurrentLineAC<CR>", { desc = "search char1 on in current line (forward)" })
-  vim.keymap.set("n", "F", "<cmd>HopChar1CurrentLineBC<CR>", { desc = "search char1 on in current line (beforeward)" })
-  vim.keymap.set("n", "<leader>w", "<cmd>HopWordAC<CR>", { desc = "search word (forward)" })
-  vim.keymap.set("n", "<leader>W", "<cmd>HopWordBC<CR>", { desc = "search word (backward)" })
-  vim.keymap.set("n", "<leader>c", "<cmd>HopWordCurrentLineAC<CR>", { desc = "search word in current line (forward)" })
-  vim.keymap.set("n", "<leader>C", "<cmd>HopWordCurrentLineBC<CR>", { desc = "search word in current line (forward)" })
-end
-
-return M
