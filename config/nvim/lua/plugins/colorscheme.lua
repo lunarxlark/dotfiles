76606d08ea069c9038b30dfedfd9992c32a7f268
lunarxlark <lunarxlark@gmail.com>
2023-01-08 22:06:00 +0900

    feat(gruvbox):delete

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -15,17 +15,31 @@ return {
       require("nightfox").load()
     end,
   },
-    config = function()
-      require("gruvbox").setup({
-        italic = false,
-        overrides = {
-          DiffAdd = { fg = "#182819", bg = "#b8bb26" },
-          DiffChange = { fg = "#182819", bg = "#8ec07c" },
-          DiffDelete = { fg = "#210A07", bg = "#fb4934" },
-          DiffText = { fg = "#291F08", bg = "#fabd2f" },
-        },
-      })
-      require("gruvbox").load()
-    end,
-  },
+  -- {
+  --   "ellisonleao/gruvbox.nvim",
+  --   lazy = false,
+  --   config = function()
+  --     require("gruvbox").setup({
+  --       italic = true,
+  --       -- inverse = true,
+  --       overrides = {
+  --         DiffAdd = { fg = "#182819", bg = "#b8bb26" },
+  --         DiffChange = { fg = "#182819", bg = "#8ec07c" },
+  --         DiffDelete = { fg = "#210A07", bg = "#fb4934" },
+  --         DiffText = { fg = "#291F08", bg = "#fabd2f" },
+  --         GitSignsChangeLnInline = { fg = "#b8bb26", bg = "#182819" },
+  --         GitSignsAddLnInline = { fg = "#b8bb26", bg = "#182819" },
+  --       },
+  --     })
+  --     -- require("gruvbox").load()
+  --   end,
+  -- },
+  -- {
+  --   "gruvbox.nvim",
+  --   dev = true,
+  --   config = function()
+  --     require("gruvbox").setup()
+  --     -- require("gruvbox").load()
+  --   end,
+  -- },
 }
