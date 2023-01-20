2c8635e8386f77f9bb9a4fdea621ec83053ffbe8
lunarxlark <lunarxlark@gmail.com>
2023-01-21 01:42:59 +0900

    feat(treesitter):add playground & refactor

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -21,6 +21,7 @@ return {
           "lua",
           "php",
           "python",
+          "query",
           "regex",
           "markdown",
           "markdown_inline",
@@ -35,13 +36,7 @@ return {
           "yaml",
         },
         sync_install = false,
-        highlight = {
-          enable = true,
-        },
-        refactor = {
-          highlight_definitions = { enable = true },
-        },
-        -- experimental
+        highlight = { enable = true },
         indent = {
           enable = true,
         },
@@ -53,6 +48,33 @@ return {
     "nvim-treesitter/nvim-treesitter-context",
     config = function()
       require("treesitter-context").setup()
+    end,
+  },
+  {
+    "nvim-treesitter/playground",
+
+    cmd = "TSPlaygroundToggle",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        playground = {
+          enable = true,
+          disable = {},
+          updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+          persist_queries = false, -- Whether the query persists across vim sessions
+          keybindings = {
+            toggle_query_editor = "o",
+            toggle_hl_groups = "i",
+            toggle_injected_languages = "t",
+            toggle_anonymous_nodes = "a",
+            toggle_language_display = "I",
+            focus_language = "f",
+            unfocus_language = "F",
+            update = "R",
+            goto_node = "<cr>",
+            show_help = "?",
+          },
+        },
+      })
     end,
   },
 }
