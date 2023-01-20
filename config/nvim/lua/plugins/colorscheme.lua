fe89a99ca05f20330f572bc5b242df0a3eaa257f
lunarxlark <lunarxlark@gmail.com>
2023-01-21 01:18:40 +0900

    refactor(colorscheme):delete catppucine

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,30 +1,5 @@
 return {
   {
-    "catppuccin/nvim",
-    config = function()
-      require("catppuccin").setup({
-        flavor = "macchiato",
-      })
-    end,
-  },
-  -- {
-  --   "EdenEast/nightfox.nvim",
-  --   config = function()
-  --     require("nightfox").setup()
-  --     vim.cmd([[colorscheme nordfox]])
-  --   end,
-  -- },
-  -- {
-  --   "gruvbox.nvim",
-  --   dev = true,
-  --   config = function()
-  --     require("gruvbox").setup()
-  --     -- vim.cmd([[colorscheme nordfox]])
-  --     -- vim.cmd([[colorscheme gruvfox]])
-  --     require("gruvbox").load()
-  --   end,
-  -- },
-  {
     "lunarxlark/nightfox.nvim",
     config = function()
       require("nightfox").setup()
