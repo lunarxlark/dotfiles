b7cd700b5090cd1c97e52dbb698d65e0c918597a
lunarxlark <lunarxlark@gmail.com>
2023-01-21 01:17:39 +0900

    fix(tig):remap keybind

--- config/nvim/lua/plugins/tig.lua
+++ config/nvim/lua/plugins/tig.lua
@@ -6,7 +6,7 @@ return {
   },
   cmd = { "TigOpenProjectRootDir", "TigBlame" },
   keys = {
-    { "<leader>T", "<cmd>TigOpenProjectRootDir<cr>", { desc = "tig open" } },
+    { "<leader>Tg", "<cmd>TigOpenProjectRootDir<cr>", { desc = "tig open" } },
     { "<leader>Tc", "<cmd>TigOpenCurrentFile<cr>", { desc = "tig open %" } },
     { "<leader>Tb", "<cmd>TigBlame<cr>", { desc = "tig blame" } },
   },
