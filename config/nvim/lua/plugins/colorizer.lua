1d211d9d0bb5d82533cb1611d8bc84f98e30057e
lunarxlark <lunarxlark@gmail.com>
2023-02-18 23:40:19 +0900

    feat:update colorizer config

--- config/nvim/lua/plugins/colorizer.lua
+++ config/nvim/lua/plugins/colorizer.lua
@@ -1,12 +1,15 @@
 return {
   "norcalli/nvim-colorizer.lua",
 
+  event = "BufReadPost",
   config = function()
     require("colorizer").setup({
       "css",
       "tsx",
       "javascript",
       "lua",
+      "toml",
+      "noice",
     })
   end,
 }
