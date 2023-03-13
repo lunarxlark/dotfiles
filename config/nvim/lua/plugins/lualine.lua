e8fece1c65aabcba871d82abf8eb58f30b82aa33
lunarxlark <lunarxlark@gmail.com>
2023-03-13 22:22:54 +0900

    chore:formatting

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -1,7 +1,6 @@
 return {
   {
     "kdheepak/tabline.nvim",
-
     keys = {
       { "<leader><leader>n", "<cmd>TablineBufferNext<cr>" },
       { "<leader><leader>p", "<cmd>TablineBufferPrevious<cr>" },
@@ -14,13 +13,13 @@ return {
           section_separators = { "", "" },
           component_separators = { "", "" },
           max_bufferline_percent = 66, -- set to nil by default, and it uses vim.o.columns * 2/3
-          show_tabs_always = true, -- this shows tabs only when there are more than one tab or if the first tab is named
-          show_devicons = true, -- this shows devicons in buffer section
-          show_bufnr = false, -- this appends [bufnr] to buffer section,
-          show_filename_only = false, -- shows base filename only instead of relative path in filename
-          modified_icon = "+ ", -- change the default modified icon
-          modified_italic = false, -- set to true by default; this determines whether the filename turns italic if modified
-          show_tabs_only = false, -- this shows only tabs instead of tabs + buffers
+          show_tabs_always = true,     -- this shows tabs only when there are more than one tab or if the first tab is named
+          show_devicons = true,        -- this shows devicons in buffer section
+          show_bufnr = false,          -- this appends [bufnr] to buffer section,
+          show_filename_only = false,  -- shows base filename only instead of relative path in filename
+          modified_icon = "+ ",        -- change the default modified icon
+          modified_italic = false,     -- set to true by default; this determines whether the filename turns italic if modified
+          show_tabs_only = false,      -- this shows only tabs instead of tabs + buffers
         },
       })
     end,
@@ -29,7 +28,6 @@ return {
   {
     "nvim-lualine/lualine.nvim",
     event = "VeryLazy",
-
     config = function()
       require("lualine").setup({
         options = {
@@ -41,9 +39,9 @@ return {
         sections = {
           lualine_a = { "mode" },
           lualine_b = {
-            { "branch", icon = "" },
-            { "filetype", colored = true, icon_only = true, separator = "", padding = { left = 1, right = 0 } },
-            { "filename", path = 1, file_status = true },
+            { "branch",   icon = "" },
+            { "filetype", colored = true, icon_only = true,  separator = "", padding = { left = 1, right = 0 } },
+            { "filename", path = 1,       file_status = true },
             { "encoding" },
             {
               "fileformat",
