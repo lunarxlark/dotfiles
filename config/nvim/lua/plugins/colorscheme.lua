d2cd91b45117226e57ed7ca84574b4f38c1a090b
lunarxlark <lunarxlark@gmail.com>
2023-05-02 21:41:11 +0900

    chore:add nightfox

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -4,8 +4,24 @@ return {
     "rktjmp/lush.nvim"
   },
   {
+    "EdenEast/nightfox.nvim",
+    enabled = false,
+    config = function()
+      local palettes = {
+        duskfox = {
+          bg1 = "#040C1C",
+        },
+      }
+      require("nightfox").setup({
+        palettes = palettes,
+      })
+      vim.cmd("colorscheme duskfox")
+    end
+  },
+  {
     "lunarxlark/futurefunk.nvim",
     dev = true,
+    enabled = true,
     config = function()
       vim.cmd([[colorscheme futurefunk]])
     end,
