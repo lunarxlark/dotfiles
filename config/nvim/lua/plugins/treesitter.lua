568d9c863108281663762a0e44855cb962567cb9
lunarxlark <lunarxlark@gmail.com>
2023-05-26 10:13:22 +0900

    chore(treesitter): add treesitter-textobjects

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -57,13 +57,15 @@ return {
 
   {
     "nvim-treesitter/playground",
-    cmd = "TSPlaygroundToggle",
+    keys = {
+      { "<leader>th", "<cmd>TSHighlightCapturesUnderCursor<cr>" },
+    },
     config = function()
       require("nvim-treesitter.configs").setup({
         playground = {
           enable = true,
           disable = {},
-          updatetime = 25,         -- Debounced time for highlighting nodes in the playground from source code
+          updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
           persist_queries = false, -- Whether the query persists across vim sessions
           keybindings = {
             toggle_query_editor = "o",
