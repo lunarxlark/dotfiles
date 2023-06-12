06b54191fdf9cd3a905ed1458c7fcd8e0153aff8
lunarxlark <lunarxlark@gmail.com>
2023-06-12 15:11:37 +0900

    nvim(wezterm):formatting

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -23,98 +23,98 @@ local HOVER_TAB_FG = "Black"
 -- end)
 
 wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
-  local background = NORMAL_TAB_BG
-  local foreground = NORMAL_TAB_FG
+	local background = NORMAL_TAB_BG
+	local foreground = NORMAL_TAB_FG
 
-  local is_first = tab.tab_id == tabs[1].tab_id
-  local is_last = tab.tab_id == tabs[#tabs].tab_id
+	local is_first = tab.tab_id == tabs[1].tab_id
+	local is_last = tab.tab_id == tabs[#tabs].tab_id
 
-  if tab.is_active then
-    background = ACTIVE_TAB_BG
-    foreground = ACTIVE_TAB_FG
-  elseif hover then
-    background = HOVER_TAB_BG
-    foreground = HOVER_TAB_FG
-  end
+	if tab.is_active then
+		background = ACTIVE_TAB_BG
+		foreground = ACTIVE_TAB_FG
+	elseif hover then
+		background = HOVER_TAB_BG
+		foreground = HOVER_TAB_FG
+	end
 
-  local leading_fg = NORMAL_TAB_FG
-  local leading_bg = background
+	local leading_fg = NORMAL_TAB_FG
+	local leading_bg = background
 
-  local trailing_fg = background
-  local trailing_bg = NORMAL_TAB_BG
+	local trailing_fg = background
+	local trailing_bg = NORMAL_TAB_BG
 
-  if is_first then
-    leading_fg = TAB_BAR_BG
-  else
-    leading_fg = NORMAL_TAB_BG
-  end
+	if is_first then
+		leading_fg = TAB_BAR_BG
+	else
+		leading_fg = NORMAL_TAB_BG
+	end
 
-  if is_last then
-    trailing_bg = TAB_BAR_BG
-  else
-    trailing_bg = NORMAL_TAB_BG
-  end
+	if is_last then
+		trailing_bg = TAB_BAR_BG
+	else
+		trailing_bg = NORMAL_TAB_BG
+	end
 
-  local title = tab.active_pane.title
+	local title = tab.active_pane.title
 
-  return {
-    { Attribute = { Italic = false } },
-    { Attribute = { Intensity = hover and "Bold" or "Normal" } },
-    { Background = { Color = leading_bg } },
-    { Foreground = { Color = leading_fg } },
-    { Text = SOLID_RIGHT_ARROW },
-    { Background = { Color = background } },
-    { Foreground = { Color = foreground } },
-    { Text = " " .. title .. " " },
-    { Background = { Color = trailing_bg } },
-    { Foreground = { Color = trailing_fg } },
-    { Text = SOLID_RIGHT_ARROW },
-  }
+	return {
+		{ Attribute = { Italic = false } },
+		{ Attribute = { Intensity = hover and "Bold" or "Normal" } },
+		{ Background = { Color = leading_bg } },
+		{ Foreground = { Color = leading_fg } },
+		{ Text = SOLID_RIGHT_ARROW },
+		{ Background = { Color = background } },
+		{ Foreground = { Color = foreground } },
+		{ Text = " " .. title .. " " },
+		{ Background = { Color = trailing_bg } },
+		{ Foreground = { Color = trailing_fg } },
+		{ Text = SOLID_RIGHT_ARROW },
+	}
 end)
 
 return {
-  --color_scheme = "gruvfox",
-  color_scheme = "futurepunk",
-  leader = { key = "t", mods = "CTRL", timeout_milliseconds = 1000 },
-  font = wezterm.font("MesloLGM Nerd Font", { weight = "Regular", stretch = "Normal", style = "Normal" }),
-  font_size = 14.0,
-  adjust_window_size_when_changing_font_size = false,
-  disable_default_key_bindings = true,
-  use_ime = true,
-  tab_bar_at_bottom = false,
-  use_fancy_tab_bar = false,
-  tab_bar_style = {
-    new_tab = wezterm.format({
-      { Background = { Color = HOVER_TAB_BG } },
-      { Foreground = { Color = TAB_BAR_BG } },
-      { Text = SOLID_RIGHT_ARROW },
-      { Background = { Color = HOVER_TAB_BG } },
-      { Foreground = { Color = HOVER_TAB_FG } },
-      { Text = " + " },
-      { Background = { Color = TAB_BAR_BG } },
-      { Foreground = { Color = HOVER_TAB_BG } },
-      { Text = SOLID_RIGHT_ARROW },
-    }),
-    new_tab_hover = wezterm.format({
-      { Attribute = { Italic = false } },
-      { Attribute = { Intensity = "Bold" } },
-      { Background = { Color = NORMAL_TAB_BG } },
-      { Foreground = { Color = TAB_BAR_BG } },
-      { Text = SOLID_RIGHT_ARROW },
-      { Background = { Color = NORMAL_TAB_BG } },
-      { Foreground = { Color = NORMAL_TAB_FG } },
-      { Text = " + " },
-      { Background = { Color = TAB_BAR_BG } },
-      { Foreground = { Color = NORMAL_TAB_BG } },
-      { Text = SOLID_RIGHT_ARROW },
-    }),
-  },
-  window_padding = {
-    top = 0,
-    bottom = 0,
-    left = 0,
-    right = 0,
-  },
-  window_decorations = "RESIZE",
-  keys = require("keymap"),
+	--color_scheme = "gruvfox",
+	color_scheme = "futurepunk",
+	leader = { key = "t", mods = "CTRL", timeout_milliseconds = 1000 },
+	font = wezterm.font("MesloLGM Nerd Font", { weight = "Regular", stretch = "Normal", style = "Normal" }),
+	font_size = 14.0,
+	adjust_window_size_when_changing_font_size = false,
+	disable_default_key_bindings = true,
+	use_ime = true,
+	tab_bar_at_bottom = false,
+	use_fancy_tab_bar = false,
+	tab_bar_style = {
+		new_tab = wezterm.format({
+			{ Background = { Color = HOVER_TAB_BG } },
+			{ Foreground = { Color = TAB_BAR_BG } },
+			{ Text = SOLID_RIGHT_ARROW },
+			{ Background = { Color = HOVER_TAB_BG } },
+			{ Foreground = { Color = HOVER_TAB_FG } },
+			{ Text = " + " },
+			{ Background = { Color = TAB_BAR_BG } },
+			{ Foreground = { Color = HOVER_TAB_BG } },
+			{ Text = SOLID_RIGHT_ARROW },
+		}),
+		new_tab_hover = wezterm.format({
+			{ Attribute = { Italic = false } },
+			{ Attribute = { Intensity = "Bold" } },
+			{ Background = { Color = NORMAL_TAB_BG } },
+			{ Foreground = { Color = TAB_BAR_BG } },
+			{ Text = SOLID_RIGHT_ARROW },
+			{ Background = { Color = NORMAL_TAB_BG } },
+			{ Foreground = { Color = NORMAL_TAB_FG } },
+			{ Text = " + " },
+			{ Background = { Color = TAB_BAR_BG } },
+			{ Foreground = { Color = NORMAL_TAB_BG } },
+			{ Text = SOLID_RIGHT_ARROW },
+		}),
+	},
+	window_padding = {
+		top = 0,
+		bottom = 0,
+		left = 0,
+		right = 0,
+	},
+	window_decorations = "RESIZE",
+	keys = require("keymap"),
 }
